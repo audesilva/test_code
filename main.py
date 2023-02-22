714cadeddb1cc59cecc7dda75bd4e5a5236779c5
Akila de Silva <audesilva@users.noreply.github.com>
2023-02-22 09:54:47 -0800

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,42 @@ def load_model_details(location):
   else:
     raise Exception("rip detector is only optimized for these locations.\n 1. currituck_hampton_inn")
   return url, weights_path, use_timex
+
+def get_prediction(img_path, model, device, RIP_INSTANCE_CATEGORY_NAMES, confidence):
+  """
+  get_prediction
+    parameters:
+      - img_path - path of the input image
+      - confidence - threshold value for prediction score
+    method:
+      - Image is obtained from the image path
+      - the image is converted to image tensor using PyTorch's Transforms
+      - image is passed through the model to get the predictions
+      - class, box coordinates are obtained, but only prediction score > threshold
+        are chosen.
+    
+  """
+  # img = Image.open(img_path)
+  img = img_path
+  transform = T.Compose([T.ToTensor()])
+  img = transform(img)
+  pred = model([img.to(device,dtype=torch.float)])
+  pred_class = [RIP_INSTANCE_CATEGORY_NAMES[i] for i in list(pred[0]['labels'].cpu().numpy())]
+  pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().cpu().numpy())]
+  pred_score = list(pred[0]['scores'].detach().cpu().numpy())
+  print("pred_score ", pred_score)
+  pred_t = [pred_score.index(x) for x in pred_score if x>confidence] 
+  if len(pred_t) == 0:
+    pred_boxes = []
+    pred_class = []
+    pred_score = []
+  else:
+    pred_t = [pred_score.index(x) for x in pred_score if x>confidence][-1]
+    pred_boxes = pred_boxes[:pred_t+1]
+    pred_class = pred_class[:pred_t+1]
+    pred_score = pred_score[:pred_t+1]
+
+  return pred_boxes, pred_class, pred_score
   
 def main():
   location = sys.argv[1]
